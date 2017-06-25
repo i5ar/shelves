88a353a6f22028f4c87ef828029d0a497f36c65a
iSar <i5ar@live.it>
2017-06-25 18:41:54 +0200

    Prepare deployment on AWS Elastic Beanstalk
    	modified:   gerp/wsgi.py
    	modified:   manage.py
    	modified:   requirements.txt
    
    	new file:   .ebextensions/db-migrate.config
    	new file:   .ebextensions/django.config
    	new file:   .elasticbeanstalk/config.yml
    	new file:   accounts/management/commands/createsu.py

--- gerp/settings/production.py
+++ gerp/settings/production.py
@@ -4,6 +4,59 @@ from .base import *
 
 DEBUG = False
 
+
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+
+
+ALLOWED_HOSTS = ['.mvsm3depy3.eu-central-1.elasticbeanstalk.com']
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+
+# S3 settings
+AWS_STORAGE_BUCKET_NAME = 'ityper'
+# User credentials from IAM
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+
+# https://github.com/jschneier/django-storages/issues/28#issuecomment-265876674
+# Tell django-storages to use signature version 4
+AWS_S3_REGION_NAME = 'eu-central-1'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+
+# Tell django-storages that when coming up with the URL for an item in
+# S3 storage, use this domain plus the path.
+# This controls how the 'static' template tag gets expanded
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# This is used by the 'static' template tag from 'static' or by anything else
+# refers directly to STATIC_URL.
+# STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+# Tell the staticfiles app to use S3 Boto 3 Storage when running 'collectstatic'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Override S3Boto3Storage in order to accomodate 'media' and 'static'
+# directories in the same bucket
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticS3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaS3Boto3Storage'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+
+
 try:
     from .local import *
 except ImportError:
