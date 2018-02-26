ad9d7b53159a71a6b1463a969b22532ba16210d8
isar <i5ar@live.it>
2018-02-26 11:37:52 +0100

    Update startsession.sh

--- multiplexer.sh
+++ multiplexer.sh
@@ -3,18 +3,11 @@
 # Start a terminal multiplexer session with separate windows for development.
 # NOTE: Link this file to your home: `ln -s ~/venv/gerp/startsession.sh ~`.
 
-# Set parameters
-venv="$HOME/venv36gerp/"
-back_end="$HOME/venv36gerp/gerp/"
-front_end="$HOME/venv36gerp/gerp-front/"
-o="--all --color --date=short"
-f="%C(yellow)%h%x20%C(cyan)%cd%C(reset)%x20%an%C(green)%d%C(reset)%x20%s"
-if [ ! -d "$venv" ] || [ ! -d "$back_end" ] || [ ! -d "$front_end" ]; then
-    echo "One or more directories are missing."; exit 1;
-fi
-
-# Check programs
+# Check required programs
 # https://stackoverflow.com/questions/592620/
+command -v tmux > /dev/null 2>&1 || {
+    echo 1>&2 "Requiere Tmux."; exit 1;
+}
 command -v chrome > /dev/null 2>&1 || {
     echo 1>&2 "Requiere Chrome."; exit 1;
 }
@@ -25,10 +18,37 @@ command -v git > /dev/null 2>&1 || {
     echo 1>&2 "Requiere Git."; exit 1;
 }
 
-# Open tabs
-chrome http://127.0.0.1:8000/en/django-admin/shelves/ &
-chrome http://127.0.0.1:8000/en/api/shelves/ &
-chrome http://127.0.0.1:9000 &
+while getopts ":bhk" opt; do
+    case $opt in
+        b)  # Open browser tabs
+            chrome http://127.0.0.1:8000/en/django-admin/shelves/ &
+            chrome http://127.0.0.1:8000/en/api/shelves/ &
+            chrome http://127.0.0.1:9000 &
+            ;;
+        h)
+            echo "usage: sh $0 [-b] [-k]"
+            exit 0;;
+        k)  # Kill all sessions like `pkill -f tmux` but better
+            tmux kill-session -a
+            tmux kill-session
+            sleep 0.5
+            tmux list-sessions
+            exit 0;;
+        *)
+            echo "usage: sh $0 [-b] [-k]"
+            exit 1;;
+    esac
+done
+
+# Set parameters
+venv="$HOME/venv36gerp/"
+back_end="$HOME/venv36gerp/gerp/"
+front_end="$HOME/venv36gerp/gerp-front/"
+o="--all --color --date=short"
+f="%C(yellow)%h%x20%C(cyan)%cd%C(reset)%x20%an%C(green)%d%C(reset)%x20%s"
+if [ ! -d "$venv" ] || [ ! -d "$back_end" ] || [ ! -d "$front_end" ]; then
+    echo "One or more directories are missing."; exit 1;
+fi
 
 # Run commands
 # https://stackoverflow.com/questions/15257130/
